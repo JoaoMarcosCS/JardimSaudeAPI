29844013e34a9d7347f88f547d837280cad9a55b
Joao Marcos <jmcsjoaomarcos@gmail.com>
2024-03-15 09:22:56 -0300

    feat: Arquvios finalizados

--- src/routes/tratamento/tratamentoRouter.ts
+++ src/routes/tratamento/tratamentoRouter.ts
@@ -2,12 +2,24 @@ import validateCreateTratamento from "../../middleware/validateCreateTratamento"
 import tratamentoController from "../../controllers/tratamento/tratamentoController";
 import { Router } from "express";
 import loginRequired from "../../middleware/loginRequired";
+import { nivelMedico } from "../../middleware/nivelMedico";
+import nivelSecretaria from "../../middleware/nivelSecretaria";
 
 const router = Router();
 
-router.post("/", validateCreateTratamento, tratamentoController.store);
+router.post(
+  "/",
+  validateCreateTratamento,
+  nivelSecretaria,
+  tratamentoController.store,
+);
 router.get("/", loginRequired, tratamentoController.index);
-router.get("/:id",loginRequired, tratamentoController.show);
-router.put("/:id/:operation",loginRequired, tratamentoController.finishTratamento);
+router.get("/:id", loginRequired, tratamentoController.show);
+router.put(
+  "/:id/:operation",
+  loginRequired,
+  nivelMedico,
+  tratamentoController.finishTratamento,
+);
 
 export default router;
