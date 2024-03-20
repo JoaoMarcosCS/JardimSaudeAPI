1d4941c8af5f17e7ca0b3574dcfb7d451107136a
João Marcos <jmcsjoaomarcos@gmail.com>
2024-03-20 20:50:28 +0000

    feat: Seed de pacientes

---
+++ src/database/seeders/Pacientes/PacienteSeed.ts
@@ -0,0 +1,23 @@
+import { Paciente } from "../../../entities/Paciente";
+import { pacientesData } from "./data/PacienteData";
+
+const pacientes = <Paciente[]>[]
+
+for(let i=0; i<pacientesData.length; i++){
+    const paciente = new Paciente();
+    paciente.altura = pacientesData[i].altura
+    paciente.bairro = pacientesData[i].bairro
+    paciente.cidade = pacientesData[i].cidade
+    paciente.cpf = pacientesData[i].cpf
+    paciente.email = pacientesData[i].email
+    paciente.nascimento = pacientesData[i].nascimento
+    paciente.nome = pacientesData[i].nome
+    paciente.rua = pacientesData[i].rua
+    paciente.sexo = pacientesData[i].sexo
+    paciente.telefone = pacientesData[i].telefone
+    paciente.uf = pacientesData[i].uf
+
+    pacientes.push(paciente);
+}
+
+export default pacientes
