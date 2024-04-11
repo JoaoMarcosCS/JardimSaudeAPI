82211609807e79efeb726a8cbf3144916b0ee20b
João Marcos <jmcsjoaomarcos@gmail.com>
2024-04-11 12:08:13 +0000

    feat: Adicionado mais origins no whiteList

--- src/config/whiteList.ts
+++ src/config/whiteList.ts
@@ -1,1 +1,5 @@
-export const whiteList = ["http://localhost:3000"];
+export const whiteList = [
+    "http://localhost:3000",
+    "https://bug-free-system-qgvxv594765c97rx-3000.app.github.dev",
+    "https://jardim-saude.vercel.app/"
+];
