d28076066bc0dba55bc55c707191cc98a0e49521
João Marcos <jmcsjoaomarcos@gmail.com>
2025-07-24 23:46:04 -0300

    Update whiteList.ts

--- src/config/whiteList.ts
+++ src/config/whiteList.ts
@@ -1,5 +1,6 @@
 export const whiteList = [
     "http://localhost:3000",
     "https://bug-free-system-qgvxv594765c97rx-3000.app.github.dev",
-    "https://jardim-saude.vercel.app/"
+    "https://jardim-saude.vercel.app/",
+    "https://keep-alive-jardim-saude-ten.vercel.app/"
 ];
